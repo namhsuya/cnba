f1f5247bc033745f202be681ce643faf53e324ca
Ayushman KB <ayushmaan.banerjee@gmail.com>
2022-12-10 19:40:13 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="cnba",
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[
+        'click',
+        'pandas',
+        'statsmodels',
+        'scipy',
+        'console_logging',
+        'tqdm'
+    ],
+    entry_points='''
+    [console_scripts]
+    cnba=cnba.cnba.__main__:main
+    '''
+
+)
