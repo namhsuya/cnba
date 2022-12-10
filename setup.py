6f7bbc7fd57a1202b4d08f0457d98681b70fc7eb
Ayushman KB <ayushmaan.banerjee@gmail.com>
2022-12-10 20:59:34 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import find_packages, setup
 setup(
     name="cnba",
     version='0.0.1',
+    author = 'Ayushman Kumar Banerjee',
+    author_email = 'ayushmaan.banerjee@gmail.com',
+    url = 'https://github.com/namhsuya/cnba/',
     packages=find_packages(),
     install_requires=[
         'click',
